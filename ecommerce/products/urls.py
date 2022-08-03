43b7ddc46f131807a7c3db48f27f6d3058d2fc5e
Alejandro Diaz Alvarado <alexndro7145@gmail.com>
2022-08-02 21:30:23 -0500

    forms and search bar

--- ecommerce/products/urls.py
+++ ecommerce/products/urls.py
@@ -1,8 +1,11 @@
+from re import search
 from django.urls import path
-from products.views import list_products, create_product
+from products.views import list_products, create_product, primer_form, search_products
 
 
 urlpatterns = [
     path('create_product/', create_product, name="create_product"),
     path('list_products/', list_products, name="list_products"),
+    path('primer_formulario/', primer_form, name="primer_formulario"),
+    path('search-products/', search_products, name="search_products"),
 ]
--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -1,21 +1,29 @@
 from itertools import product
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 from products.models import Products
-
+from products.forms import Formulario_products
 # Create your views here.
 
 def create_product(request):
-    new_product = Products.objects.create(
-        name = "Coca Cola 1l",
-        price = 20,
-        stock = 20
-    )
 
-    context = {
-        'new_product': new_product
-    }
+    if request.method == 'POST':
+        form = Formulario_products(request.POST)
 
-    return render(request, 'products.html', context=context)
+        if form.is_valid():
+            Products.objects.create(
+                name = form.cleaned_data['name'],
+                price = form.cleaned_data['price'],
+                description = form.cleaned_data['description'],
+                stock = form.cleaned_data['stock']
+            )
+
+            return redirect(list_products)
+
+    elif request.method == 'GET':
+        form = Formulario_products()
+        context = {'form': form}
+        return render(request, 'products.html', context=context)
 
 def list_products(request):
     products = Products.objects.all()
@@ -25,3 +33,20 @@ def list_products(request):
     }
 
     return render(request, 'products_list.html', context=context)
+
+def primer_form(request):
+    print(request.method)
+
+    if request.method == 'POST':
+        print(request.POST)
+
+    return render(request, 'primer_form.html', context={})
+
+def search_products(request):
+    search = request.GET['search']
+    products = Products.objects.filter(name__icontains=search)
+    context = {
+        'products': products
+    }
+
+    return render(request, 'search_product.html', context=context)
